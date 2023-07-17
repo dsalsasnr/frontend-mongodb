4671f14378306acfd653bc475a94e07c236e1bc0
dsalsasnr <dsalsasnr@gmail.com>
2023-07-17 08:58:47 +0700

    frontend untuk crud orm mongoose

--- src/pages/Edit/index.jsx
+++ src/pages/Edit/index.jsx
@@ -1,21 +1,71 @@
+import React, { useState, useEffect } from "react";
+import { useParams, useNavigate } from "react-router-dom";
+import axios from "axios";
 import Input from "../../components/Input";
 
 const Edit = () => {
+  const { id } = useParams();
+  const navigate = useNavigate();
+  const [name, setName] = useState("");
+  const [price, setPrice] = useState("");
+  const [stock, setStock] = useState("");
+  const [status, setStatus] = useState(false);
+  const [image, setImage] = useState(null);
+
+  useEffect(() => {
+    const fetchProduct = async () => {
+      try {
+        const response = await axios.get(`/api/v4/product/${id}`);
+        const { name, price, stock, status } = response.data;
+        setName(name);
+        setPrice(price);
+        setStock(stock);
+        setStatus(status);
+      } catch (error) {
+        console.error(error);
+      }
+    };
+
+    fetchProduct();
+  }, [id]);
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    try {
+      const formData = new FormData();
+      formData.append("name", name);
+      formData.append("price", price);
+      formData.append("stock", stock);
+      formData.append("status", status);
+      formData.append("image", image);
+
+      await axios.put(`/api/v4/product/${id}`, formData);
+      navigate(`/detail/${id}`);
+      console.log("Data berhasil diupdate");
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
   return (
     <div className="main">
       <div className="card">
         <h2>Edit Produk</h2>
         <br />
-        <form>
-          <Input name="name" type="text" placeholder="Nama Produk..." label="Nama" value="test"/>
-          <Input name="price" type="number" placeholder="Harga Produk..." label="Harga" value="20000000"/>
-          <Input name="Stock" type="number" placeholder="Stock Produk..." label="Stock" value="10"/>
-          <Input name="status" type="checkbox" label="Active" checked/>
-          <button type="submit" className="btn btn-primary">Simpan</button>
+        <form onSubmit={handleSubmit}>
+          <Input name="name" type="text" placeholder="Nama Produk..." label="Nama" value={name} onChange={(e) => setName(e.target.value)} />
+          <Input name="price" type="number" placeholder="Harga Produk..." label="Harga" value={price} onChange={(e) => setPrice(e.target.value)} />
+          <Input name="Stock" type="number" placeholder="Stock Produk..." label="Stock" value={stock} onChange={(e) => setStock(e.target.value)} />
+          <Input name="status" type="checkbox" label="Active" checked={status} onChange={(e) => setStatus(e.target.checked)} />
+          <input type="file" onChange={(e) => setImage(e.target.files[0])} />
+          <button type="submit" className="btn btn-primary">
+            Simpan
+          </button>
         </form>
       </div>
     </div>
-  )
-}
+  );
+};
 
 export default Edit;
--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,12 +1,41 @@
-import { Link } from 'react-router-dom';
-import './index.scss';
+import { Link } from "react-router-dom";
+import { useState, useEffect } from "react";
+import axios from "axios";
+import "./index.scss";
 
 const Home = () => {
-  return(
+  const [products, setProducts] = useState([]);
+
+  useEffect(() => {
+    const fetchProducts = async () => {
+      try {
+        const response = await axios.get("http://localhost:3001/api/v4/product");
+        setProducts(response.data);
+      } catch (error) {
+        console.error(error);
+      }
+    };
+
+    fetchProducts();
+  }, []);
+
+  const handleDelete = async (id) => {
+    try {
+      await axios.delete(`http://localhost:3001/api/v4/product/${id}`);
+      console.log("Product deleted successfully");
+      fetchProducts();
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  return (
     <div className="main">
-      <Link to="/tambah" className="btn btn-primary">Tamah Produk</Link>
+      <Link to="/tambah" className="btn btn-primary">
+        Tambah Produk
+      </Link>
       <div className="search">
-        <input type="text" placeholder="Masukan kata kunci..."/>
+        <input type="text" placeholder="Masukkan kata kunci..." />
       </div>
       <table className="table">
         <thead>
@@ -18,30 +47,28 @@ const Home = () => {
           </tr>
         </thead>
         <tbody>
-          <tr>
-            <td>1</td>
-            <td>Laptop</td>
-            <td className="text-right">RP. 20.000.000</td>
-            <td className="text-center">
-              <Link to="/detail" className="btn btn-sm btn-info">Detail</Link>
-              <Link to="/edit" className="btn btn-sm btn-warning">Edit</Link>
-              <Link to="#" className="btn btn-sm btn-danger">Delete</Link>
-            </td>
-          </tr>
-          <tr>
-            <td>2</td>
-            <td>Monitor</td>
-            <td className="text-right">RP. 10.000.000</td>
-            <td className="text-center">
-              <Link to="/detail" className="btn btn-sm btn-info">Detail</Link>
-              <Link to="/edit" className="btn btn-sm btn-warning">Edit</Link>
-              <Link to="#" className="btn btn-sm btn-danger">Delete</Link>
-            </td>
-          </tr>
+          {products.map((product) => (
+            <tr key={product._id}>
+              <td>{product._id}</td>
+              <td>{product.name}</td>
+              <td className="text-right">{product.price}</td>
+              <td className="text-center">
+                <Link to={`/detail/${product._id}`} className="btn btn-sm btn-info">
+                  Detail
+                </Link>
+                <Link to={`/edit/${product._id}`} className="btn btn-sm btn-warning">
+                  Edit
+                </Link>
+                <button className="btn btn-sm btn-danger" onClick={() => handleDelete(product._id)}>
+                  Delete
+                </button>
+              </td>
+            </tr>
+          ))}
         </tbody>
       </table>
     </div>
-  )
-}
+  );
+};
 
 export default Home;
